e4977f6bd27f9533c386fe33c3b4b39eeb8274b0
Zebulun Arendsee <zbwrnz@gmail.com>
2021-12-20 02:58:03 -0600

    Move tests to unittest framework

---
+++ test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import unittest
+import msalign as m
+
+def align(query, subject):
+    return m.NeedlemanWunsch(query, subject).get_alignment()
+
+class TestAlignments(unittest.TestCase):
+
+    def test_base_cases(self):
+        self.assertEqual(align(query="", subject=""), "")
+        self.assertEqual(align(query="CAT", subject=""), "")
+        self.assertEqual(align(query="", subject="CAT"), "---")
+
+    def test_simple(self):
+        self.assertEqual(align(query="M", subject="M"), "M")
+        self.assertEqual(align(query="M", subject="MMM"), "--M")
+        self.assertEqual(align(query="WM", subject="WMMM"), "WM--")
+        self.assertEqual(align(query="WMAM", subject="WMMM"), "WMAM")
+
+    def test_problematic(self):
+        self.assertEqual(align(query="ATTTTY", subject="ATTTT-IT"), "ATTTT--Y")
+        self.assertEqual(align(query="ATTTTY", subject="ATTTT--IT"), "ATTTT---Y")
+
+    def test_errors(self):
+        with self.assertRaises(ValueError):
+            align(query="CAT-IN-HAT", subject="CAT")
+
+        with self.assertRaises(ValueError):
+            align(query="BONE", subject="CAT")
+
+        with self.assertRaises(ValueError):
+            align(query="CAT", subject="BONE")
+
+if __name__ == '__main__':
+    unittest.main()
